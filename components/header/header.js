69c7e5d32bf503e1084c5dea03f9949d1b5cd2e8
josephwilliams <josephwilli12@gmail.com>
2017-01-08 17:18:30 -0800

    better spacing

---
+++ components/header/header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import HeaderImage from './headerImage';
+
+const Header = () => (
+  <div className="header-wrapper">
+    <HeaderImage />
+  </div>
+);
+
+export default Header;
---
+++ components/splashVideo.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const SplashVideo = () => (
+  <div className="splash-video-wrapper">
+    {'embedded music video'}
+  </div>
+);
+
+export default SplashVideo;
