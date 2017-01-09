b671f70e7208f55f8dd3622cf7479970eed38b84
josephwilliams <josephwilli12@gmail.com>
2017-01-08 17:56:29 -0800

    react-youtube implementation begun

--- components/splashVideo.js
+++ components/splashVideo.js
@@ -1,9 +1,36 @@
 import React from 'react';
 
-const SplashVideo = () => (
-  <div className="splash-video-wrapper">
-    {'embedded music video'}
-  </div>
-);
+// https://www.npmjs.com/package/react-youtube
+import Youtube from 'react-youtube';
+// <YouTube
+//   videoId="eVW1qOmTG4I"
+//   opts={opts}
+//   onReady={this._onReady}
+// />
 
-export default SplashVideo;
+export default class SplashVideo extends React.Component {
+  constructor () {
+    super();
+  }
+
+  _onReady(event) {
+    // access to player in all event handlers via event.target
+    event.target.pauseVideo();
+  }
+
+  render () {
+    const opts = {
+      height: '300',
+      width: '400',
+      playerVars: { // https://developers.google.com/youtube/player_parameters
+        autoplay: 0,
+      },
+    };
+
+    return (
+      <div className="splash-video-wrapper">
+        {'embedded video'}
+      </div>
+    );
+  }
+}
